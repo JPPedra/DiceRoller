6caec2fd054128dfbd5576a2c79f0684a38cca8b
Joaopedra <1732576@brunel.ac.uk>
2019-10-24 11:50:23 +0100

    Part IIIa: Let's play D-ICEBREAKERS! Level 4 done.

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = this.findViewById(R.id.numberTextView);
         Random r = new Random();
-        int number = r.nextInt(6);
+        int number = roll_the_dice();
 
 
         tv.setText(Integer.toString(number));
@@ -52,4 +52,33 @@ public class MainActivity extends AppCompatActivity {
         }
         }
 
+    public int roll_the_dice() {
+
+        Random r = new Random();
+        int numberondice = r.nextInt(6);
+        return numberondice;
+
+    }
+
+        public void buttonclick2(View view) {
+
+            TextView tv2 = this.findViewById(R.id.mensagem);
+            //tv2.setText(Integer.toString(numberondice));
+            TextView mensagem = this.findViewById(R.id.mensagem);
+
+            int r = roll_the_dice();
+
+            if (r == 0) {
+                mensagem.setText("If you could go anywhere in the world, where would you go?\n");}
+            if (r == 1) {
+                mensagem.setText("If you were stranded on a desert island, what three things would you want to take with you?\n");}
+            if (r == 2) {
+                mensagem.setText("If you could eat only one food for the rest of your life, what would that be?\n");}
+            if (r == 3) {
+                mensagem.setText("If you won a million dollars, what is the first thing you would buy?\n");}
+            if (r == 4) {
+                mensagem.setText("If you could spaned the day with one fictional character, who would it be?\n");}
+            if (r == 5) {
+                mensagem.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?\n");}
+        }
     }
